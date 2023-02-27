79199b548d8a5a04419093cc640157fa78d42150
liuhaicheng <884024720@qq.com>
2023-02-27 13:01:21 +0800

    双色球结果追踪

--- src/main/java/com/github/binarywang/demo/wx/mp/WxMpDemoApplication.java
+++ src/main/java/com/github/binarywang/demo/wx/mp/WxMpDemoApplication.java
@@ -2,11 +2,13 @@ package com.github.binarywang.demo.wx.mp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * @author <a href="https://github.com/binarywang">Binary Wang</a>
  */
 @SpringBootApplication
+@EnableScheduling
 public class WxMpDemoApplication {
 
   public static void main(String[] args) {
