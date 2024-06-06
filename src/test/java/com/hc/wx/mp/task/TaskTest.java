a2b5a8eac5b3f0fd4a6e7807d24e63bb2fd33f4b
liuhaicheng <hc.liu987@gmail.com>
2024-06-06 22:02:15 +0800

    查询代码优化

--- src/test/java/com/hc/wx/mp/task/TaskTest.java
+++ src/test/java/com/hc/wx/mp/task/TaskTest.java
@@ -26,9 +26,9 @@ class TaskTest {
 
     @Test
     void appointmentResults() throws InterruptedException, WxErrorException {
-      //  FileReader fileReader = FileReader.create(new File("/Users/liuhaicheng/Desktop/1.txt"));
+        //  FileReader fileReader = FileReader.create(new File("/Users/liuhaicheng/Desktop/1.txt"));
         //String[] split = fileReader.readString().split("\n");
-      // redisTemplate.opsForList().rightPushAll("sf",split);
+        // redisTemplate.opsForList().rightPushAll("sf",split);
 
     }
 
@@ -39,39 +39,9 @@ class TaskTest {
 //        jsonObject.put("value","2");
 //
 //        System.out.println(jsonObject.toString());
-     //  String auth ="Bearer eyJhbGciOiJIUzM4NCIsInR5cCI6IkpXVCJ9.eyJkYXRhIjoiLWY5M0xtZ0M3X0RfcTFCLXZKNmNaMDM2MXFNd3YwazdPcU9JMWo2SmhPdlpzX2gzeloiLCJpYXQiOjE3MTcwNTU1NTksImV4cCI6MTcxODc4MzU1OX0.wIjJu9j2ilYGcFj8sSFrCCeCApli6-tI5vpzTXQkK53eWKoA256Bi0iJWKlrAq1t";
-      // redisTemplate.opsForValue().set("auth",auth);
+        //  String auth ="Bearer eyJhbGciOiJIUzM4NCIsInR5cCI6IkpXVCJ9.eyJkYXRhIjoiLWY5M0xtZ0M3X0RfcTFCLXZKNmNaMDM2MXFNd3YwazdPcU9JMWo2SmhPdlpzX2gzeloiLCJpYXQiOjE3MTcwNTU1NTksImV4cCI6MTcxODc4MzU1OX0.wIjJu9j2ilYGcFj8sSFrCCeCApli6-tI5vpzTXQkK53eWKoA256Bi0iJWKlrAq1t";
+        // redisTemplate.opsForValue().set("auth",auth);
     }
-
-    private void processSubList(List<String> results) {
-
-        OkHttpClient client = new OkHttpClient();
-
-
-        String requestBody = "{\"name\":\"sfsyUrl\",\"value\":\"884024720\",\"remarks\":null,\"id\":1}";
-        JSONObject jsonObject = JSONUtil.parseObj(requestBody);
-
-        Request request = new Request.Builder()
-                .url("http://139.196.92.81:5700/api/envs?t=1717033389419")
-                .put(RequestBody.create(requestBody.getBytes()))
-                .header("Host", "139.196.92.81:5700")
-                .header("Accept", "application/json, text/plain, */*")
-                .header("Authorization", "Bearer eyJhbGciOiJIUzM4NCIsInR5cCI6IkpXVCJ9.eyJkYXRhIjoiLTVfVk5BcDl1WUh5U21SUmpkcXk2QkhpVFYyWW9QcHpjMlpGaEZVbkE5emJxVkxRSndDMVFjV3R5M0p0dEdTIiwiaWF0IjoxNzE1MzIxODM1LCJleHAiOjE3MTcwNDk4MzV9.89aDecMgtwoaI_X-PMu1Ca9-e-XpMDS2HnY7_WZPpUForZz0hLTuvOa1Af96SQ2j")
-                .header("Accept-Language", "zh-CN,zh-Hans;q=0.9")
-                .header("Content-Type", "application/json")
-                .header("Origin", "http://139.196.92.81:5700")
-                .header("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.5 Safari/605.1.15")
-                .header("Referer", "http://139.196.92.81:5700/env")
-                .build();
-
-        try (Response response = client.newCall(request).execute()) {
-            if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
-            response.body().string();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
 
 
 }
